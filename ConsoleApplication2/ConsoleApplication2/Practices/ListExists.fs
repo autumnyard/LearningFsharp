9813e0d0c942a2bb2deafcb4e0b0f9558ad7b1ed
AutumnYard <autumnyard@protonmail.ch>
2022-06-12 20:40:40 +0200

    Beginning to implement a Game Inventory

---
+++ ConsoleApplication2/ConsoleApplication2/Practices/ListExists.fs
@@ -0,0 +1,27 @@
+﻿module ListExists
+
+
+// =================================
+// Inventory
+// =================================
+
+// Working out a complex Contains/Exists, that works with complex types
+type Item = Apple | Juice | Peanut | Water
+type Entry = { Item : Item ; Quantity : int }
+let hasItem list item = (List.exists (fun x -> x.Item = item) list)
+
+let test1 = [1 ; 3 ; 5]
+printfn "Test1 Correct %b" (List.exists ((=)3) test1)
+printfn "Test1 Wrong %b" (List.exists ((=)4) test1)
+
+let test2 = [ Apple; Water]
+printfn "Test2 Correct %b" (List.exists ((=)Apple) test2)
+printfn "Test2 Wrong %b" (List.exists ((=)Peanut) test2)
+
+let test3 = [ {Item = Apple ; Quantity = 2} ; {Item = Water ; Quantity = 4} ]
+//printfn "Test3 Correct %b" (List.exists (fun x -> x.Item = Apple) test3)
+printfn "Test3 Correct %b" (hasItem test3 Apple)
+//printfn "Test3 Wrong %b" (List.exists (fun x -> x.Item = Peanut) test3)
+//printfn "Test3 Wrong %b" (List.exists (hasItem Peanut) test3)
+printfn "Test3 Wrong %b" (hasItem test3 Peanut)
+
---
+++ ConsoleApplication2/ConsoleApplication2/WinterInMere.fs
@@ -0,0 +1,95 @@
+﻿module WinterInMere
+
+// The idea is to practice using the systems implemented in my first published game
+// Resources:
+//    https://fsharpforfunandprofit.com/posts/correctness-exhaustive-pattern-matching/
+
+
+// =================================
+// Data
+// =================================
+
+type Item = Apple | Juice | Peanut | Water
+
+
+// =================================
+// Inventory
+// =================================
+
+type Entry = { Item : Item ; Quantity : int }
+type ActiveInventory = {Items : Entry list}
+type EmptyInventory = NoItems
+// Is a FullInventory state necessary?
+
+type Inventory =
+    | Empty of EmptyInventory
+    | Active of ActiveInventory
+
+
+// =================================
+// operations on empty state
+// =================================
+
+let addToEmpty item =
+    Inventory.Active {Items = [item]}
+    
+// =================================
+// operations on active state
+// =================================
+
+//let hasItem item = fun x -> x.Item = item
+let hasItem list item = (List.exists (fun x -> x.Item = item) list)
+
+let addToActiveForce state item =
+    let newList = item :: state.Items
+    Inventory.Active { state with Items = newList }
+
+let addToActive state item =
+    let newList = item :: state.Items
+    match (hasItem state.Items item.Item) with
+        | true -> 
+            Inventory.Active state
+        | false -> 
+            Inventory.Active {state with Items = newList}
+
+type EmptyInventory with
+    member this.Add = addToEmpty
+
+type ActiveInventory with
+    member this.Add = addToActive this
+
+    
+// =================================
+// operations on inventory
+// =================================
+
+let addItemToInventory (inventory:Inventory) item =
+    match inventory with
+    | Empty state -> state.Add item
+    | Active state -> state.Add item
+
+let displayInventory inventory =
+    match inventory with
+    | Empty state -> printfn "The inventory is empty"
+    | Active state -> printfn "The inventory contains %A" state.Items
+
+type Inventory with
+    static member NewInventory = Inventory.Empty NoItems
+    member this.Add = addItemToInventory this
+    member this.Display = displayInventory this
+    
+
+// =================================
+// testing
+// =================================
+
+let emptyInventory = Inventory.NewInventory
+printf "\nempty="; emptyInventory.Display
+
+let inventoryState1 = emptyInventory.Add {Item=Apple;Quantity=2}
+printfn "\nstate1="; inventoryState1.Display
+
+let add i q (x:Inventory) = x.Add {Item=i;Quantity=q}
+
+let inventoryState2 = Inventory.NewInventory |> add Apple 2 |> add Peanut 3
+printfn "\nstate2="; inventoryState2.Display
